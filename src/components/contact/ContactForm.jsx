c11e46cec79ce717683b677e90a79663f19dc404
yashiikaaaa <yashika@truestate.in>
2025-06-05 15:16:15 +0530

    number change

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -187,8 +187,8 @@ const ContactForm = ({ contactmodal, setContactModal, setSiteVisitModal }) => {
               </div>
               <div className="mx-auto max-w-sm w-full">
                 <button className="text-white my-5 p-2 w-full bg-PrestigeBrown flex items-center justify-center hover:bg-opacity-90 transition">
-                  <a href="tel:+919036958110" className="flex items-center">
-                    <Phone className="w-5 h-5 mr-2" />9036958110
+                  <a href="tel:+916364254487" className="flex items-center">
+                    <Phone className="w-5 h-5 mr-2" />63642 54487
                   </a>
                 </button>
               </div>
